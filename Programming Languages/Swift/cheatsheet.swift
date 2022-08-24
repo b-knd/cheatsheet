9d14fd3f76bb4c7fcd4e900d1edd4b9ae5d64a45
jingru <104969882+b-knd@users.noreply.github.com>
2022-08-24 10:59:10 +0800

    Update cheatsheet.swift

--- Programming Languages/Swift/cheatsheet.swift
+++ Programming Languages/Swift/cheatsheet.swift
@@ -1,1 +1,21 @@
 
+//importing framework
+import UIKit
+
+//defining string variable
+var str = "Hello, world!"
+
+//url
+var url = NSURL(string: "http://www.craiggrummitt.com")
+
+//loop
+for x in 0..<10 {
+    x * x
+}
+
+//color
+var color:UIColor = .purple
+
+//control
+//slider
+var slider = UISlider(frame: CGRect(x: 0, y: 0, width: 100, height: 50))
