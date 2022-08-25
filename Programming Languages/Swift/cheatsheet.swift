87ddf27c3f14f581f4e33fa55bb6c611bd4c4474
jingru <104969882+b-knd@users.noreply.github.com>
2022-08-25 10:21:33 +0800

    Update cheatsheet.swift

--- Programming Languages/Swift/cheatsheet.swift
+++ Programming Languages/Swift/cheatsheet.swift
@@ -2,21 +2,91 @@
 import UIKit
 
 /*
-Type safety and type inference:
+2. COLLECTIONS
 */
 
-//declaring variable
-var str = "Hello, playground" //without specifying data type, better practice
-var feedback: String //specifying data type (if value is not assigned to it yet)
+//ARRAYS (storing same data type in ordered list
+//--declaring
+var array:[String] = []
+//--instantiating
+array = ["hello", "world"]
+//--adding element
+array.insert("!", at: 2) //specifying index
+array.append("!") //without index
+//--interating
+//without index
+for i in array {
+    print(i)
+}
+//with index
+for (index, i) in array.enumerated(){
+    print("No. \(index+1): \(i)")
+}
+//--extracting element
+let i = array.first
+let j = array[1]
+//--extracting range
+let firstTwo = array[0...1]
+//--concatenating
+let p = ["It's a great day!"]
+array = p + array
+
+//SETS
+//--declaration
+var sets:Set<String>
+//--initialization
+sets = ["A", "B", "C"]
+//--adding and removing element
+sets.insert("D")
+sets.remove("D")
+//--iteration
+for i in sets {
+    print(i)
+}
+//--combining sets
+var anotherSets:Set = ["D", "E", "F", "G"]
+var unionOfTwo = sets.union(anotherSets) //some other combination includes: intersection, symmetric difference and subtraction
+
+//DICTIONARIES
+
+//____________________________________________________________________________________________________________________________________________//
+
+/*
+1. TYPE SAFETY AND TYPE INFERENCE:
+*/
+
+//DECLARING VARIABLES
+//1.without specifying data type, better practice
+var str = "Hello, playground" 
+//2.specifying data type (if value is not assigned to it yet)
+var feedback: String 
 if paymentSuccessful {
     feedback = "Payment Successful!"
 } else {
     feedback = "Unsuccessful!"
 }
+//3.clarifying data types while declaring
+var quantitySoup:Double = 2
 
 
-//defining string variable
-var str = "Hello, world!"
+//CONVERTING NUMERIC TYPES
+var totalSlices = 8
+var numberOfPeople = 3
+var slicesPerPerson = Double(totalSlices) / Double(numberOfPeople) //without converting, the division returns 2 instead of double
+//some other data types: UInt(unsigned integer), Float, Double, Int
+
+
+//CONCATENATING STRINGS
+var name = "Jerry"
+var message = "Welcome " + name //only if name is string
+//preventing type safe error
+//1. convert data types
+var cost = 3.50
+var message = "This costs $" + String(cost)
+//2. string interpolation
+var message = "This costs $\(cost)"
+
+
 
 //url
 var url = NSURL(string: "http://www.craiggrummitt.com")
